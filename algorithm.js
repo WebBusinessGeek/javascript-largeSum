7078ce435306beeaff833915aeecac99fc955389
Kevin Washington <kevw12188@gmail.com>
2015-09-28 11:15:36 -0400

    added breakStringIntoArray method to algorithm class

--- algorithm.js
+++ algorithm.js
@@ -1,3 +1,22 @@
 function Algorithm() {
-    
+    this.testsAreWorkingStatus = false;
+
+    this.setTestsAreWorkingStatus = function(value) {
+        if(value == true || value == false) {
+            this.testsAreWorkingStatus = value;
+        }
+    };
+
+    this.getTestsAreWorkingStatus = function() {
+        return this.testsAreWorkingStatus;
+    };
+
+    this.breakStringIntoArray = function(stringToBreakUp, nthPosition) {
+        var returnValue = [];
+        for (var i = 0; i < stringToBreakUp.length; i += nthPosition) {
+            returnValue.push(stringToBreakUp.substring(i, i + nthPosition));
+        }
+        return returnValue;
+    };
+
 }
---
+++ algorithm.test.js
@@ -0,0 +1,39 @@
+describe("tests are working", function() {
+    it("should return true", function() {
+        var algo = new Algorithm();
+        algo.setTestsAreWorkingStatus(true);
+        var result = algo.getTestsAreWorkingStatus();
+        var correct = true;
+        expect(result).toEqual(correct);
+    });
+});
+
+
+describe("breakStringIntoArray", function() {
+    it("should break up '123456789' into an array with 3 elements when argument is 3 ", function() {
+        var stringToBreakUp = "123456789";
+        var nthPosition = 3;
+        var algo = new Algorithm();
+        var result = algo.breakStringIntoArray(stringToBreakUp, nthPosition);
+        var correct = [];
+        correct.push("123");
+        correct.push("456");
+        correct.push("789");
+        expect(result).toEqual(correct);
+    });
+    it("should break up '41122411224112241122411224112241122' into an array with 6 elements when argument is 5", function() {
+        var stringToBreakUp = "41122411224112241122411224112241122";
+        var nthPosition = 5;
+        var algo = new Algorithm();
+        var result = algo.breakStringIntoArray(stringToBreakUp, nthPosition);
+        var correct = [];
+        correct.push("41122");
+        correct.push("41122");
+        correct.push("41122");
+        correct.push("41122");
+        correct.push("41122");
+        correct.push("41122");
+        correct.push("41122");
+        expect(result).toEqual(correct);
+    })
+});
